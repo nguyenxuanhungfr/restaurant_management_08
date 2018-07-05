4d683d795531bf79ff8bdf743168fa80a7d688ec
huuhau <nguyenhuuhau1061995@gmail.com>
2018-07-05 19:15:49 +0700

    sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,38 @@ class User < ApplicationRecord
   has_many :dishes, through: :reviews
   has_many :bookings
   has_many :tables, through: :bookings
+
+  before_save :downcase_email
+  attr_accessor :remember_token
+  validates :name, presence: true,
+   length: {maximum: Settings.validate.name_max_length}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+    length: {maximum: Settings.validate.email_max_length},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, allow_nil: true,
+    length: {minimum: Settings.validate.min_length_password}
+
+  has_secure_password
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  class << self
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
 end
