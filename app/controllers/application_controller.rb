9a942e5b355c8e6a2dbaf2fc31916ee900e8c022
hungxuan <nguyen.xuan.hung@framgia.com>
2018-07-06 14:40:11 +0700

    view_menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  def load_menu
+    @menus = Menu.all
+  end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,5 @@
 class StaticPagesController < ApplicationController
+  before_action :load_menu
+
   def home; end
 end
