294ae7ffe51c9350fe3c7baffd0b1e65172fe7f9
hungxuan <nguyen.xuan.hung@framgia.com>
2018-07-06 08:24:58 +0700

    design-homepage_admin

--- app/assets/javascripts/admin/admin.js
+++ app/assets/javascripts/admin/admin.js
@@ -1,10 +1,7 @@
 /*
 //= require jquery
 //= require bootstrap
-//= require toastr
 //= require rails-ujs
 //= require admin/dashboard.js
-//= require cocoon
-//= require jquery.turbolinks
 //= require_self
 */
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root "static_pages#home"
+  namespace :admin do
+    root "static_pages#home"
+  end
 end
