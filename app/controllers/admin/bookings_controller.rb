5c5b340e7dbb5b7ce5b544d30a31b12565015cdf
hungxuan <nguyen.xuan.hung@framgia.com>
2018-07-12 16:52:01 +0700

    admin_search_booking

--- app/controllers/admin/bookings_controller.rb
+++ app/controllers/admin/bookings_controller.rb
@@ -2,6 +2,6 @@ class Admin::BookingsController < Admin::BaseController
   before_action :logged_in_user
 
   def index
-    @bookings = Booking.page(params[:page]).per Settings.settings.per_page
+    @bookings = Booking.search_by_type(params[:search]).page(params[:page]).per Settings.settings.per_page
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,6 +5,8 @@ class Booking < ApplicationRecord
   has_many :dishes, through: :booking_details
   has_many :bills
 
-  delegate :name, :to => :user, :prefix => true
-  delegate :number_of_people, :to => :table, :prefix => true
+  delegate :name, to: :user, prefix: true
+  delegate :number_of_people, to: :table, prefix: true
+
+  scope :search_by_type, ->(name){where("name LIKE ? or phone LIKE ?", "%#{name}%", "%#{name}%") if name.present?}
 end
