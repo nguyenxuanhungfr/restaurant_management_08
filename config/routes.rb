a7c3351afe113797bce958d49118dd3d684214e0
huuhau <nguyenhuuhau1061995@gmail.com>
2018-07-13 10:45:55 +0700

    admin manager table

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,6 @@ Rails.application.routes.draw do
     resources :bookings
     resources :menus
     resources :categories
+    resources :tables
   end
 end
---
+++ db/migrate/20180716033506_add_name_string_to_tables.rb
@@ -0,0 +1,5 @@
+class AddNameStringToTables < ActiveRecord::Migration[5.1]
+  def change
+     add_column :tables, :name, :string
+  end
+end
